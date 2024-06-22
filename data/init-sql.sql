b26095aa961690e5c715f1e4e3f0e36c0ea8fe8a
Florencia Rios <florencia.rios@MacBook277.local>
2024-06-22 12:23:43 -0300

    Migre a postgree

--- data/init-sql.sql
+++ data/init-sql.sql
@@ -1,116 +1,102 @@
-CREATE DATABASE [boletinesoficiales]
-GO
+CREATE EXTENSION dblink;
 
-USE [boletinesoficiales]
-GO
+DO $$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_database
+      WHERE datname = 'boletinesoficiales'
+   ) THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE boletinesoficiales');
+   END IF;
+END
+$$;
 
-CREATE SCHEMA [core]
-GO
+\c boletinesoficiales
 
-CREATE SCHEMA [user]
-GO
+CREATE TABLE IF NOT EXISTS cargos (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(30),
+  codigo VARCHAR(2)
+);
 
-CREATE TABLE [core].[cargos] (
-  [id] INTEGER PRIMARY KEY IDENTITY(1, 1),
-  [nombre] varchar(30),
-  [codigo] varchar(2)
-)
-GO
+CREATE TABLE IF NOT EXISTS estado_civil (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(30),
+  codigo VARCHAR(1)
+);
 
-CREATE TABLE [core].[estado_civil] (
-  [id] INTEGER PRIMARY KEY IDENTITY(1, 1),
-  [nombre] varchar(30),
-  [codigo] varchar(1)
-)
-GO
+CREATE TABLE IF NOT EXISTS fuente_informacion (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(30),
+  codigo VARCHAR(3)
+);
 
-CREATE TABLE [core].[fuente_informacion] (
-  [id] INTEGER PRIMARY KEY IDENTITY(1, 1),
-  [nombre] varchar(30),
-  [codigo] varchar(3)
-)
-GO
+CREATE TABLE IF NOT EXISTS provincias (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(30),
+  codigo VARCHAR(1)
+);
 
-CREATE TABLE [core].[provincias] (
-  [id] INTEGER PRIMARY KEY IDENTITY(1, 1),
-  [nombre] varchar(30),
-  [codigo] varchar(1)
-)
-GO
+CREATE TABLE IF NOT EXISTS sexo (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(30),
+  codigo VARCHAR(1)
+);
 
-CREATE TABLE [core].[sexo] (
-  [id] INTEGER PRIMARY KEY IDENTITY(1, 1),
-  [nombre] varchar(30),
-  [codigo] varchar(1)
-)
-GO
+CREATE TABLE IF NOT EXISTS nacionalidades (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(30),
+  codigo VARCHAR(2)
+);
 
-CREATE TABLE [core].[nacionalidades] (
-  [id] INTEGER PRIMARY KEY IDENTITY(1, 1),
-  [nombre] varchar(30),
-  [codigo] varchar(2)
-)
-GO
+CREATE TABLE IF NOT EXISTS sociedad (
+  id SERIAL PRIMARY KEY,
+  mor_user VARCHAR(10),
+  mor_nro_user VARCHAR(10),
+  mor_lote VARCHAR(10),
+  mor_codint INT,
+  mor_matriz VARCHAR(10),
+  mor_sucursal VARCHAR(10),
+  mor_sector VARCHAR(10),
+  mor_cliente VARCHAR(10),
+  mor_nombre_completo VARCHAR(72),
+  mor_fecha_nac VARCHAR(10),
+  mor_sexo_id INT,
+  mor_documento1 VARCHAR(11),
+  mor_documento2 VARCHAR(11),
+  mor_prov_doc2 VARCHAR(1),
+  mor_telefono VARCHAR(14),
+  mor_marca_dire_1 VARCHAR(1),
+  mor_calle VARCHAR(40),
+  mor_numer VARCHAR(10),
+  mor_piso VARCHAR(6),
+  mor_loca VARCHAR(36),
+  mor_prov_id INT,
+  mor_cp VARCHAR(8),
+  mor_est_civil_id INT,
+  mor_nacionalidad_id INT,
+  mor_relacion VARCHAR(1),
+  mor_cargo_id INT,
+  mor_cargo_fecha VARCHAR(10),
+  mor_cargo_fuente_id INT,
+  mor_ant_codigo VARCHAR(3) DEFAULT 'XXX',
+  mor_campo_1 VARCHAR(20),
+  mor_campo_2 VARCHAR(20),
+  mor_campo_3 VARCHAR(20),
+  mor_campo_4 VARCHAR(20),
+  mor_campo_5 VARCHAR(20),
+  mor_campo_6 VARCHAR(20),
+  mor_campo_7 VARCHAR(20),
+  mor_campo_8 VARCHAR(20),
+  mor_ant_fecha VARCHAR(10),
+  mor_archivo VARCHAR(72),
+  mor_soc_categoria VARCHAR(3),
+  fecha_insercion_boletin VARCHAR
+);
 
-CREATE TABLE [user].[sociedad] (
-    [id] INTEGER PRIMARY KEY IDENTITY(1, 1),
-    [mor_user] VARCHAR(10),
-    [mor_nro_user] VARCHAR(10),
-    [mor_lote] VARCHAR(10),
-    [mor_codint] INT,
-    [mor_matriz] VARCHAR(10),
-    [mor_sucursal] VARCHAR(10),
-    [mor_sector] VARCHAR(10),
-    [mor_cliente] VARCHAR(10),
-    [mor_nombre_completo] VARCHAR(72),
-    [mor_fecha_nac] VARCHAR(10),
-    [mor_sexo_id] INTEGER,
-    [mor_documento1] VARCHAR(11),
-    [mor_documento2] VARCHAR(11),
-    [mor_prov_doc2] VARCHAR(1),
-    [mor_telefono] VARCHAR(14),
-    [mor_marca_dire_1] VARCHAR(1),
-    [mor_calle] VARCHAR(40),
-    [mor_numer] VARCHAR(10),
-    [mor_piso] VARCHAR(6),
-    [mor_loca] VARCHAR(36),
-    [mor_prov_id] INTEGER,
-    [mor_cp] VARCHAR(8),
-    [mor_est_civil_id] INTEGER,
-    [mor_nacionalidad_id] INTEGER,
-    [mor_relacion] VARCHAR(1),
-    [mor_cargo_id] INTEGER,
-    [mor_cargo_fecha] VARCHAR(10),
-    [mor_cargo_fuente_id] INTEGER,
-    [mor_ant_codigo] VARCHAR(3) DEFAULT 'XXX',
-    [mor_campo_1] VARCHAR(20),
-    [mor_campo_2] VARCHAR(20),
-    [mor_campo_3] VARCHAR(20),
-    [mor_campo_4] VARCHAR(20),
-    [mor_campo_5] VARCHAR(20),
-    [mor_campo_6] VARCHAR(20),
-    [mor_campo_7] VARCHAR(20),
-    [mor_campo_8] VARCHAR(20),
-    [mor_ant_fecha] VARCHAR(10),
-    [mor_archivo] VARCHAR(72),
-    [mor_soc_categoria] VARCHAR(3)
-)
-GO
-
-ALTER TABLE [user].[sociedad] ADD FOREIGN KEY ([mor_sexo_id]) REFERENCES [core].[sexo]([id])
-GO
-
-ALTER TABLE [user].[sociedad] ADD FOREIGN KEY ([mor_prov_id]) REFERENCES [core].[provincias]([id])
-GO
-
-ALTER TABLE [user].[sociedad] ADD FOREIGN KEY ([mor_est_civil_id]) REFERENCES [core].[estado_civil]([id])
-GO
-
-ALTER TABLE [user].[sociedad] ADD FOREIGN KEY ([mor_nacionalidad_id]) REFERENCES [core].[nacionalidades]([id])
-GO
-
-ALTER TABLE [user].[sociedad] ADD FOREIGN KEY ([mor_cargo_id]) REFERENCES [core].[cargos]([id])
-GO
-
-ALTER TABLE [user].[sociedad] ADD FOREIGN KEY ([mor_cargo_fuente_id]) REFERENCES [core].[fuente_informacion]([id])
-GO
+ALTER TABLE sociedad ADD FOREIGN KEY (mor_sexo_id) REFERENCES sexo(id);
+ALTER TABLE sociedad ADD FOREIGN KEY (mor_prov_id) REFERENCES provincias(id);
+ALTER TABLE sociedad ADD FOREIGN KEY (mor_est_civil_id) REFERENCES estado_civil(id);
+ALTER TABLE sociedad ADD FOREIGN KEY (mor_nacionalidad_id) REFERENCES nacionalidades(id);
+ALTER TABLE sociedad ADD FOREIGN KEY (mor_cargo_id) REFERENCES cargos(id);
+ALTER TABLE sociedad ADD FOREIGN KEY (mor_cargo_fuente_id) REFERENCES fuente_informacion(id);
