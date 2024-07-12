660e26241a1185162e9377b5da2e05190d4fba2a
Florencia Rios <florencia.rios@MacBook277.local>
2024-07-11 23:25:09 -0300

    Cambie a postgree, actulice la icializacion de la base de datos

--- data/init-sql.sql
+++ data/init-sql.sql
@@ -1,55 +1,42 @@
-CREATE EXTENSION dblink;
-
 DO $$
 BEGIN
-   IF NOT EXISTS (
-      SELECT FROM pg_database
-      WHERE datname = 'boletinesoficiales'
-   ) THEN
-      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE boletinesoficiales');
-   END IF;
+   PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE boletinesoficiales');
 END
 $$;
 
 \c boletinesoficiales
 
-CREATE TABLE IF NOT EXISTS cargos (
+CREATE TABLE cargos (
   id SERIAL PRIMARY KEY,
   nombre VARCHAR(30),
   codigo VARCHAR(2)
 );
 
-CREATE TABLE IF NOT EXISTS estado_civil (
+CREATE TABLE estado_civil (
   id SERIAL PRIMARY KEY,
   nombre VARCHAR(30),
   codigo VARCHAR(1)
 );
 
-CREATE TABLE IF NOT EXISTS fuente_informacion (
-  id SERIAL PRIMARY KEY,
-  nombre VARCHAR(30),
-  codigo VARCHAR(3)
-);
-
-CREATE TABLE IF NOT EXISTS provincias (
+CREATE TABLE provincias (
   id SERIAL PRIMARY KEY,
   nombre VARCHAR(30),
   codigo VARCHAR(1)
 );
 
-CREATE TABLE IF NOT EXISTS sexo (
+CREATE TABLE sexo (
   id SERIAL PRIMARY KEY,
   nombre VARCHAR(30),
   codigo VARCHAR(1)
 );
 
-CREATE TABLE IF NOT EXISTS nacionalidades (
+CREATE TABLE nacionalidades (
   id SERIAL PRIMARY KEY,
   nombre VARCHAR(30),
   codigo VARCHAR(2)
 );
 
-CREATE TABLE IF NOT EXISTS sociedad (
+CREATE TABLE sociedad (
   id SERIAL PRIMARY KEY,
   mor_user VARCHAR(10),
   mor_nro_user VARCHAR(10),
@@ -78,8 +65,8 @@ CREATE TABLE IF NOT EXISTS sociedad (
   mor_relacion VARCHAR(1),
   mor_cargo_id INT,
   mor_cargo_fecha VARCHAR(10),
-  mor_cargo_fuente_id INT,
-  mor_ant_codigo VARCHAR(3) DEFAULT 'XXX',
+  mor_cargo_fuente VARCHAR(3),
+  mor_ant_codigo VARCHAR(3),
   mor_campo_1 VARCHAR(20),
   mor_campo_2 VARCHAR(20),
   mor_campo_3 VARCHAR(20),
@@ -91,7 +78,8 @@ CREATE TABLE IF NOT EXISTS sociedad (
   mor_ant_fecha VARCHAR(10),
   mor_archivo VARCHAR(72),
   mor_soc_categoria VARCHAR(3),
-  fecha_insercion_boletin VARCHAR
+  fecha_insercion_boletin VARCHAR,
+  boletin_oficial BYTEA
 );
 
 ALTER TABLE sociedad ADD FOREIGN KEY (mor_sexo_id) REFERENCES sexo(id);
@@ -99,4 +87,103 @@ ALTER TABLE sociedad ADD FOREIGN KEY (mor_prov_id) REFERENCES provincias(id);
 ALTER TABLE sociedad ADD FOREIGN KEY (mor_est_civil_id) REFERENCES estado_civil(id);
 ALTER TABLE sociedad ADD FOREIGN KEY (mor_nacionalidad_id) REFERENCES nacionalidades(id);
 ALTER TABLE sociedad ADD FOREIGN KEY (mor_cargo_id) REFERENCES cargos(id);
-ALTER TABLE sociedad ADD FOREIGN KEY (mor_cargo_fuente_id) REFERENCES fuente_informacion(id);
+
+-- Inserts para la tabla cargos
+INSERT INTO cargos (nombre, codigo) VALUES
+('AB', 'ABSORBIDA'),
+('GT', 'GERENTE'),
+('DT', 'Director Titular'),
+('PR', 'Presidente'),
+('LR', 'Representante Legal'),
+('SA', 'Socio Solidario'),
+('SB', 'Socio Comanditado'),
+('SC', 'Socio Comanditario'),
+('SG', 'Socio Gerente'),
+('SO', 'UNICAMENTE PARA SOCIEDADES DE HECHO Y COLECTIVA'),
+('DA', 'DENOMINACION ANTERIOR'),
+('ES', 'ESCINDIDA'),
+('V0', 'Vicepresidente'),
+('V1', 'Vicepresidente Primero'),
+('V2', 'Vicepresidente Segundo'),
+('V3', 'Vicepresidente Tercero'),
+('V4', 'Vicepresidente Cuarto'),
+('ZZ', 'Fallecido'),
+('FU', 'FUSION'),
+('UT', 'UTE');
+
+-- Inserts para la tabla estado_civil
+INSERT INTO estado_civil (nombre, codigo) VALUES 
+('S', 'Soltero'),
+('C', 'Casado'),
+('D', 'Divorciado');
+
+-- Inserts para la tabla provincias
+INSERT INTO provincias (nombre, codigo) VALUES
+('A', 'SALTA'),
+('B', 'BUENOS AIRES'),
+('C', 'CAPITAL FEDERAL'),
+('D', 'SAN LUIS'),
+('E', 'ENTRE RIOS'),
+('F', 'LA RIOJA'),
+('G', 'SANTIAGO DEL ESTERO'),
+('H', 'CHACO'),
+('J', 'SAN JUAN'),
+('K', 'CATAMARCA'),
+('L', 'LA PAMPA'),
+('M', 'MENDOZA'),
+('N', 'MISIONES'),
+('P', 'FORMOSA'),
+('Q', 'NEUQUEN'),
+('R', 'RIO NEGRO'),
+('S', 'SANTA FE'),
+('T', 'TUCUMAN'),
+('U', 'CHUBUT'),
+('V', 'TIERRA DEL FUEGO'),
+('W', 'CORRIENTES'),
+('X', 'CORDOBA'),
+('Y', 'JUJUY'),
+('Z', 'SANTA CRUZ');
+
+-- Inserts para la tabla sexo
+INSERT INTO sexo (nombre, codigo) VALUES
+('MASCULINO', 'M'),
+('FEMENINO', 'F'),
+('NO APORTADO', 'I'),
+('SOCIEDAD', 'S');
+
+-- Inserts para la tabla nacionalidades
+INSERT INTO nacionalidades (nombre, codigo) VALUES
+('A', 'ARGENTINA'),
+('AL', 'ALEMANIA'),
+('AU', 'AUSTRALIA'),
+('BO', 'BOLIVIA'),
+('BR', 'BRASIL'),
+('C', 'COLOMBIA'),
+('CB', 'CUBA'),
+('CD', 'CANADA'),
+('CE', 'CHECOSLOVAQUIA'),
+('CH', 'CHILE'),
+('CN', 'CHINA'),
+('E', 'ESPAÑA'),
+('EC', 'ECUADOR'),
+('F', 'FRANCIA'),
+('GB', 'GRAN BRETAÑA'),
+('H', 'HOLANDA'),
+('I', 'ITALIA'),
+('IR', 'IRLANDA'),
+('J', 'JAPON'),
+('K', 'KOREA'),
+('M', 'MEXICO'),
+('O', 'EXTRANJERO'),
+('P', 'PERU'),
+('PG', 'PORTUGAL'),
+('PR', 'PARAGUAY'),
+('S', 'SUECIA'),
+('SZ', 'SUIZA'),
+('TW', 'TAIWAN'),
+('U', 'URUGUAY'),
+('US', 'ESTADOS UNIDOS'),
+('XX', 'EXTRANJERO'),
+('YU', 'YUGOSLAVIA');
+
+COMMIT;
